5aae78f3d8c946e4691ba09de323327e8eea2563
MD FAIZAN KHAN <124388165+THEFZNKHAN@users.noreply.github.com>
2024-01-13 19:57:48 +0530

    Add react-router-dom package and implement basic routing
    * Setup Tailwind CSS
    
    * Add .prettierrc
    
    * Add react-router-dom package and implement basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
     );
 }
 
