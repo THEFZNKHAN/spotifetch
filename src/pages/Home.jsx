036db9cff72eeac3b33d3491aab200c335f7021c
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-01-24 23:10:42 +0530

    Make Responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,8 +41,8 @@ const Home = () => {
     };
 
     return (
-        <div className="bg-slate-900 h-screen">
-            <div className="container flex flex-col justify-center items-center">
+        <div className="bg-neutral-900 h-screen">
+            <div className="container w-screen flex justify-center items-center">
                 {isDownloading && (
                     <Alert
                         variant="filled"
@@ -61,24 +61,16 @@ const Home = () => {
                         Error occurred. Please try again later.
                     </Alert>
                 )}
-                <div className="m-10 py-4 px-16 rounded-xl bg-white text-center border-4 border-green-900">
-                    <h1 className="text-3xl text-green-600 font-bold font-mono">
-                        Download Spotify Songs
-                    </h1>
-                    <p className="text-lg mt-4 font-serif">
-                        Download Spotify song by pasting the URL of that song.
-                    </p>
-                </div>
-                <div className="w-1/2 h-64 rounded-xl bg-whit flex flex-col justify-center items-center bg-white border-4 border-green-900">
+                <div className="mt-8 w-3/4 py-8 rounded-xl bg-white flex flex-col justify-center items-center border-4 border-green-800">
                     <input
                         type="text"
                         name="text"
                         placeholder="Enter the URLs"
-                        className="h-14 w-2/3 p-4 font-xl font-semibold text-white bg-slate-500 border-2 border-black rounded-lg transition hover:bg-slate-600"
+                        className="h-14 w-4/5 sm:w-3/4 p-4 font-xl text-sm sm:text-lg font-semibold text-white bg-slate-500 border-2 border-black rounded-lg transition hover:bg-slate-600"
                         onChange={handleChange}
                     />
                     <button
-                        className="h-14 w-1/3 mt-2 text-xl font-bold text-white bg-green-700 border-2 border-black rounded-lg transition hover:bg-green-800"
+                        className="h-14 w-4/5 sm:w-3/4 mt-2 text-sm sm:text-lg font-bold text-white bg-green-700 border-2 border-black rounded-lg transition hover:bg-green-800"
                         onClick={handleSubmit}
                     >
                         Download
